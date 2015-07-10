00166ff2e07c40143b081160ab32f7231b34f677
Daniela Zapata <dzapatariesco@gmail.com>
2015-07-10 10:06:27 -0500

    First commit: generate app with Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flapperNewsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flapperNewsApp
+ */
+angular.module('flapperNewsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
