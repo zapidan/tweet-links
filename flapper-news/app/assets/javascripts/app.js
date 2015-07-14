9d49e21f84dee39bf285db6ec833013a692e5ad1
Daniela Zapata <dzapatariesco@gmail.com>
2015-07-14 16:42:01 -0500

    Add login/logout/register functionality

--- flapper-news/app/assets/javascripts/app.js
+++ flapper-news/app/assets/javascripts/app.js
@@ -36,11 +36,31 @@ angular
         templateUrl: 'posts/_post.html',
         controller: 'PostsCtrl',
         resolve: {
-        post1: ['$stateParams', 'posts', function($stateParams, posts) {
-            return posts.get($stateParams.id);
+          post1: ['$stateParams', 'posts', function($stateParams, posts) {
+              return posts.get($stateParams.id);
+          }]
+        }
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function() {
+            $state.go('home');
+          })
         }]
-}
-    });
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function() {
+            $state.go('home');
+          })
+        }]
+      });
 
     $urlRouterProvider.otherwise('home');
 }]);
